815cbf617bf19201ef96a373815fa82b8547dcee
Patrik Ragnarsson <patrik@starkast.net>
2016-08-10 13:46:28 +0200

    use remote jekyll-import

--- import.rb
+++ import.rb
@@ -4,6 +4,7 @@ JekyllImport::Importers::Tumblr.run({
   "url"            => "http://klubbdinmamma.com",
   "format"         => "html", # "html" or "md"
   "grab_images"    => true,   # whether to download images as well.
+  "image_class"    => "news-photo", # add this class to the <img> tag
   "add_highlights" => false,  # whether to wrap code blocks (indented 4 spaces) in a Liquid "highlight" tag
   "rewrite_urls"   => false,  # whether to write pages that redirect from the old Tumblr paths to the new Jekyll paths
 })
